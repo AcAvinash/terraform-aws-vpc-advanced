774e853ef70f142bd0edcbd6fca41866ef43a894
Avinash Kumar Singh <acavinashchauhan5@gmail.com>
2025-09-21 08:35:52 +0530

    updating jenkinsfile

--- peering.tf
+++ peering.tf
@@ -1,11 +1,18 @@
+### VPC Peering with Default VPC
 resource "aws_vpc_peering_connection" "peering" {
   count = var.is_peering_required ? 1 : 0
-  auto_accept   = true
-  # peer_vpc_id is the VPC ID of the accepter VPC.
+  #peer_owner_id = var.peer_owner_id
   peer_vpc_id   = aws_vpc.main.id
-  # Requester VPC ID.
-
+  #requestor, default VPC is our requestor
   vpc_id        = var.requestor_vpc_id
+  auto_accept   = true
+
+  tags = merge(
+    {
+      Name = "${var.project_name}-${var.env}"
+    },
+    var.common_tags
+  )
 }
 
 resource "aws_route" "default_route" {
@@ -17,6 +24,7 @@ resource "aws_route" "default_route" {
   #depends_on                = [aws_route_table.testing]
 }
 
+#add route in roboshop public route table
 resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
   route_table_id            = aws_route_table.public.id
@@ -25,6 +33,7 @@ resource "aws_route" "public_peering" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
   #depends_on                = [aws_route_table.testing]
 }
+
 
 resource "aws_route" "private_peering" {
   count = var.is_peering_required ? 1 : 0
